f72ed090088ea706ca64b42e085ce009691098e3
kwrona05 <kwrona05@gmail.com>
2024-06-06 13:53:46 +0200

    Create script.ts

---
+++ script.ts
@@ -0,0 +1,21 @@
+const btn1: HTMLButtonElement | null = document.querySelector('.button1');
+const btn:  HTMLButtonElement | null = document.querySelector('.button2');
+const btn3 = document.querySelector('.button3');
+
+const paragraph = document.querySelector('.text');
+
+function text1() {
+    paragraph.textContent = "If you see this text, it means that my program is good"
+};
+
+function text2() {
+    paragraph.textContent = "Wow! My program is really good"
+};
+
+function text3() {
+    paragraph.textContent = "Nice! I love coding"
+};
+
+btn1.addEventListener('click', text1);
+btn2.addEventListener('click', text2);
+btn3.addEventListener('click', text3);
